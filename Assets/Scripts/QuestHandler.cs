3841241f0b20a3446cedd0e8d9954d9f3b5dd9d8
Superfriend <joseph.m.white@outlook.com>
2023-10-14 12:20:00 -0500

    Optimizations, added quest manager for more easily swapping

---
+++ Assets/Scripts/QuestHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class QuestHandler : MonoBehaviour
+{
+    public Torch[] flashlights;
+    public GameObject[] questOnly;
+    public GameObject[] pcOnly;
+
+    public bool quest = false;
+
+    [ContextMenu("Update Quest-PC Objects")]
+    public void UpdateObjects() {
+        foreach (Torch flashlight in flashlights) {
+            flashlight.quest = quest;
+        }
+        foreach (GameObject questObject in questOnly) {
+            questObject.SetActive(quest);
+        }
+        foreach (GameObject pcObject in pcOnly) {
+            pcObject.SetActive(!quest);
+        }
+    }
+
+}
